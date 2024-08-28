d4b991d3290e9aac97c66ca85ba92ea09bcca869
nicocanta20 <nicocanta20@gmail.com>
2024-08-28 17:06:20 -0300

    fix(component): improve ChatView style

--- components/ui/ChatView.tsx
+++ components/ui/ChatView.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { Message as MessageType } from 'ai';
 import Message from '@/components/ui/Message';
 import ChatForm from '@/components/ui/ChatForm';
+import NiceCards from '@/components/ui/NiceCards';
 import ActivityCards from '@/components/ui/ActivityCards';
+import { MiraiIcon } from '@/components/brand/MiraiIcon';
 
 type HandleSubmitType = (e: React.FormEvent<HTMLFormElement>) => void;
 type HandleInputChangeType = (
@@ -27,18 +29,42 @@ export const ChatView: React.FC<ChatViewProps> = ({
   handleSendMessage,
 }) => {
   const conversationStarted = messages.length > 0;
+  const messagesEndRef = useRef<HTMLDivElement>(null);
 
   // Define your title and description here
-  const title = "Welcome to Programming 1";
-  const description = "Here you'll find various programming topics and exercises. Select a topic to get started.";
+  const subject = "Inteligencia Artificial y Neurociencias";
+  const title = "Bienvenido a IA y Neurociencias";
+  const description = "Aquí encontrarás varios temas y ejercicios sobre los cuales podrás hacer preguntas.";
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [messages]);
+
+  const scrollToBottom = () => {
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
 
   return (
-    <div className="flex flex-col h-full w-full mx-auto stretch">
-      <div className="flex-grow overflow-y-auto">
-        <div className="max-w-3xl mx-auto py-24 space-y-4">
-          {/* Display title and description */}
+    <div className="relative flex flex-col h-full w-full mx-auto stretch">
+      {/* Label with border at the top */}
+      {conversationStarted && (
+       <div className="flex justify-center mt-10">
+       <div className="bg-white rounded-full px-6 py-3 shadow-lg mb-4">
+         <div className="flex items-center space-x-2">
+           <h2 className="text-lg font-semibold text-gray-900">{subject}</h2>
+         </div>
+       </div>
+     </div>
+      )}
+
+      <div className="flex-grow overflow-y-auto pt-10">
+        <div className="max-w-[48rem] mx-auto py-10 space-y-4">
+        {/* Display title and description */}
           {!conversationStarted && (
-            <div className="text-center mb-8 mt-20">
+            <div className="text-center mb-8 mt-10">
+              <div className="flex justify-center mb-4">
+                <MiraiIcon width={64} height={64} />
+              </div>
               <h1 className="text-4xl font-normal mb-2">{title}</h1>
               <p className="text-lg text-gray-600">{description}</p>
             </div>
@@ -52,19 +78,21 @@ export const ChatView: React.FC<ChatViewProps> = ({
               null
             )
           ))}
+          
+          <div ref={messagesEndRef} /> {/* This empty div is used as a scroll target */}
         </div>
       </div>
 
       {/* Cards */}
       {!conversationStarted && (
-        // <div className="flex justify-center w-full my-8">
-        //   <Cards onCardClick={handleSendMessage} />
-        // </div>
         <div className="flex justify-center w-full my-8">
-          <div className="max-w-4xl w-full pr-4 pl-4">
-            <ActivityCards handleSendMessage={handleSendMessage} />
-          </div>
+          <NiceCards onCardClick={handleSendMessage} />
         </div>
+        // <div className="flex justify-center w-full my-8">
+        //   <div className="max-w-4xl w-full pr-4 pl-4">
+        //     <ActivityCards handleSendMessage={handleSendMessage} />
+        //   </div>
+        // </div>
       )}
 
       {/* Chat Form */}
