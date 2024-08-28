076defb85c89f676ca2754338cb0f53bb61ccdbc
nicocanta20 <nicocanta20@gmail.com>
2024-08-28 17:01:30 -0300

    fix(ui): add icon to AI assistant message

--- components/ui/Message.tsx
+++ components/ui/Message.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Message as MessageType } from 'ai';
 import MarkdownRender from '../MarkdownRender';
+import { MiraiIcon } from '@/components/brand/MiraiIcon';
 
 type MessageProps = {
   message: MessageType;
@@ -8,22 +9,28 @@ type MessageProps = {
 
 const Message: React.FC<MessageProps> = ({ message }) => {
   return (
-    (<div
+    <div
       className={`whitespace-pre-wrap flex ${
         message.role === 'user' ? 'justify-end' : 'justify-start'
       }`}
     >
+      {message.role === 'assistant' && (
+        <div className="w-8 h-8 rounded-full bg-white border-2 flex-shrink-0 mr-3 flex items-center justify-center mt-3">
+          <MiraiIcon width={18} height={18} />
+        </div>
+      )}
       <div
         className={`p-4 inline-block max-w-xl ${
           message.role === 'user'
             ? 'bg-gray-100 text-black rounded-3xl'
-            : 'bg-white text-black rounded-xl'
+            : 'bg-white text-black rounded-xl pl-2'
         }`}
       >
         <MarkdownRender>{message.content}</MarkdownRender>
       </div>
-    </div>)
+    </div>
   );
 };
 
 export default Message;
+
