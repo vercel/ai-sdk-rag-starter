3db9aeb4b89f23dac19dcbeb5eaa0a3d0918fe6b
Nico Albanese <nico.albanese@vercel.com>
2024-10-28 12:31:32 +0000

    update resources file to match guide

--- lib/actions/resources.ts
+++ lib/actions/resources.ts
@@ -1,25 +1,24 @@
-"use server";
+'use server';
 
 import {
   NewResourceParams,
   insertResourceSchema,
   resources,
-} from "@/lib/db/schema/resources";
-import { db } from "../db";
+} from '@/lib/db/schema/resources';
+import { db } from '../db';
 
 export const createResource = async (input: NewResourceParams) => {
   try {
-    const payload = insertResourceSchema.parse(input);
+    const { content } = insertResourceSchema.parse(input);
 
-    const contentWithoutLineBreaks = payload.content.replace("\n", " ");
     const [resource] = await db
       .insert(resources)
-      .values({ content: contentWithoutLineBreaks })
+      .values({ content })
       .returning();
 
-    return "Resource successfully created.";
+    return 'Resource successfully created.';
   } catch (e) {
     if (e instanceof Error)
-      return e.message.length > 0 ? e.message : "Error, please try again.";
+      return e.message.length > 0 ? e.message : 'Error, please try again.';
   }
 };
